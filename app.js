36d9c128a0b322dfcaec46361ff4a732b3cfd6b9
Nink <74396204+ninkstan@users.noreply.github.com>
2022-07-19 19:53:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const submit = document.querySelector(".submit");
 //turn red when invalid
 emailInput.onkeydown = function () {
     if (regex.test(emailInput.value)) {
+        invalid.innerText = "";
+        inputbox.style.borderColor = "";
     } else {
         invalid.innerText = "Please provide a valid email address";
         inputbox.style.borderColor = 'red';
 
         submit.addEventListener("click", function(event) {
         event.preventDefault();
-        //console.log("Please provide a valid email address");
+        console.log("Please provide a valid email address");
         })
     }    
 }
