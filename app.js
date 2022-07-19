866cc53fc3312eb12dfcf9898b52caf618673216
Nink <74396204+ninkstan@users.noreply.github.com>
2022-07-19 19:34:28 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//email
+const emailInput = document.querySelector(".email-input");
+const invalid = document.querySelector(".invalid");
+const inputbox = document.querySelector("input");
+const regex = /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+//submit
+const submit = document.querySelector(".submit");
+
+//turn red when invalid
+emailInput.onkeydown = function () {
+    if (regex.test(emailInput.value)) {
+    } else {
+        invalid.innerText = "Please provide a valid email address";
+        inputbox.style.borderColor = 'red';
+
+        submit.addEventListener("click", function(event) {
+        event.preventDefault();
+        //console.log("Please provide a valid email address");
+        })
+    }    
+}
